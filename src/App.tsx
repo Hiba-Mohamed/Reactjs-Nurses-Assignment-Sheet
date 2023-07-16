db72b9408ecf5d058e46cdc011615a1736cad53d
Mitchell Hynes <me@mitchellhynes.com>
2023-07-16 18:51:02 -0230

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-	<div className="bg-red-400">
-		Hello world	
-        </div>
-  )
+  return <div className="bg-red-400">Hello world</div>;
 }
 
-export default App
+export default App;
+
