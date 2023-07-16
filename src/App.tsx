d9e1d2d150221d51e4db1dfd7f04f7229b0d510f
Mitchell Hynes <me@mitchellhynes.com>
2023-07-16 18:50:25 -0230

    Fixed something strange with tailwind (?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className='bg-red-300'>
-    <h1>Hello World!</h1>
-    </div>
-    </>
+	<div className="bg-red-400">
+		Hello world	
+        </div>
   )
 }
 
