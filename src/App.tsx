902ed754625f8228274d0302cf874087926ad004
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-17 13:54:48 -0230

    simple header component text success

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Heading from "./components/header/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="bg-red-400">Hello world</div>;
+  return <div>
+    <Heading />
+    </div>;
 }
 
 export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+
+export function Heading () 
+{
+    return (
+      <div>
+        <h1>
+            Hello
+        </h1>
+      </div>
+    );
+  }
+
+  export default Heading
