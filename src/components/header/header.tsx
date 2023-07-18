249e94de18acc3009f8986bc6df5b8acd63bb1bc
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-18 16:46:24 -0230

    styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,16 +20,16 @@ export function Heading() {
                 <div className={`${open ? "block" : "hidden"} w-full`} id="navbar-hamburger">
                     <ul className="flex flex-col items-center font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                         <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Home</a>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600 hover:text-green" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Create an Account</a>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-green">Create an Account</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Login</a>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-green">Login</a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">About NAS</a>
+                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-green">About NAS</a>
                         </li>
                     </ul>
                 </div>
