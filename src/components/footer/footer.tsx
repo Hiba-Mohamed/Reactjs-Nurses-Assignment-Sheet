382e19bb9851ca5780a9643bb1b1c920baa42332
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-20 22:40:19 -0230

    welcome page pic added, styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,7 @@ export function Footer() {
         <footer className="bg-green font-nunito py-8">
             <div className="text-white">
             <p className="block text-center">NAS Created by Hiba Mohamed</p>
+            <p className="block text-center">NAS Created using React.js, Node.js, and Canva</p>
             <span className="block text-center">© 2023 All Rights Reserved.</span>
             </div>  
         </footer>
--- src/home.tsx
+++ src/home.tsx
@@ -18,15 +18,17 @@ export function Home() {
   return (
     <div className="bg-cover bg-repeat bg-opacity-10" style={{ backgroundImage: `url('images/banner-pic.png')` }}>
 
-      <div className="font-nunito font-bold flex flex-row relative max-w-5xl mx-auto pt-20 sm:pt-12 lg:pt-18">
+      <div className="font-nunito flex flex-row relative max-w-5xl mx-auto">
 
         <div className="flex flex-col items-center w-screen mb-8">
+          <img src="./images/NAS-logo.png" className='w-20' alt="" />
+          <img src="./images/nurse-pic.png" className='w-80' alt="" />
 
-          <h1 className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white p-6">Welcome to the NAS (Nurses Assignment Sheet)<br></br>Generate electronic Nurses Assignment Sheets now</h1>
+          <p className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">Generate electronic Nurses Assignment Sheets now using NAS</p>
 
           <div className="flex items-center">
-            <button onClick={navigateToAccount} className="bg-lgreen hover:bg-green text-white font-bold py-4 px-8 rounded m-10 text-xl">Create Account</button>
-            <button onClick={navigateLogin} className="bg-green hover:bg-lgreen text-white font-bold py-4 px-8 rounded m-10 text-xl">Login</button>
+            <button onClick={navigateToAccount} className="bg-lgreen hover:bg-green text-white font-bold py-4 px-8 rounded m-10 lg:text-xl sm:text-l">Create Account</button>
+            <button onClick={navigateLogin} className="bg-green hover:bg-lgreen text-white font-bold py-4 px-8 rounded m-10 lg:text-xl sm:text-l">Login</button>
           </div>
 
         </div>
