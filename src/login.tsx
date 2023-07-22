f4ca3e5acb2de7cd29003ab17dbe94bd35025fd5
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-21 22:04:46 -0230

    login page styling completed

--- src/login.tsx
+++ src/login.tsx
@@ -1,8 +1,42 @@
 
-export function Login()
-{
-    return(
-        <h1>This is LOGIN page</h1>
+export function Login() {
+    return (
+        <div className="font-nunito relative flex flex-1 flex-col overflow-hidden justify-center items-center mb-12" style={{ backgroundImage: `url('images/banner-pic.png')` }}>
+
+            <div className="">
+
+                <form action="" className="w-screen max-w-sm">
+                    <div className="">
+
+                        <div className="flex flex-col items-center">
+                            <img src="./images/NAS-logo.png" className="w-24" alt="" />
+                            <h2 className="mt-4 text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem]">Log In</h2>
+                        </div>
+
+                        <div className="mb-4">
+                        <label className="block font-semibold leading-6 text-gray-900 lg:text-xl sm:text-l">Email address</label>
+                        <input type="email" id="email" className="mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200" required value=""></input>
+                        </div>
+                        <div className="">
+                        <label className="block font-semibold leading-6 text-gray-900 lg:text-xl sm:text-l">Password</label>
+                        <input type="email" id="email" className="mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200" required value=""></input>
+                        </div>
+                        <div className="">
+                            <button className="inline-flex justify-center rounded-lg font-semibold py-2.5 px-4 bg-nunito-900 text-white hover:bg-nunito-700 w-full bg-green mt-6 lg:text-xl sm:text-l">Login</button>
+                            <button className="inline-flex justify-center rounded-lg font-semibold py-2.5 px-4 bg-nunito-900 text-white hover:bg-nunito-700 w-full bg-green mt-6 mb-2 lg:text-xl sm:text-l">Login as a guest</button>
+                        </div>
+                        <div className="flex items-center flex-col">
+                            <a className="hover:text-green" href="#">Forgot password?</a>
+                            <div className="flex flex-col items-center mt-6">
+                                <a className="lg:text-xl sm:text-l hover:text-green" href="#">No account?</a>
+                                <button className="inline-flex justify-center rounded-lg font-semibold py-2.5 px-4 bg-nunito-900 text-white hover:bg-nunito-700 w-full bg-green mb-6 lg:text-xl sm:text-l">Create an Account</button>
+                            </div>  
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     )
 }
 
