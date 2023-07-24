00f8ffc1b780047cd3b89f5bc8f50a04d9ce1e63
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-23 22:28:18 -0230

    preventing selection of past days

--- src/datePickerComponent.tsx
+++ src/datePickerComponent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+
 const DatePickerComponent: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
@@ -9,12 +10,21 @@ const DatePickerComponent: React.FC = () => {
     setSelectedDate(date);
   };
 
+  // Function to disable past dates (including today)
+  const disablePastDates = (date: Date) => {
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0 for accurate comparison
+    return date >= currentDate;
+  };
+
   return (
     <div>
+      <h3 className="text-xl font-bold mb-4">Date Picker Example</h3>
       <DatePicker
         selected={selectedDate}
         onChange={handleDateChange}
         className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        filterDate={disablePastDates} // Apply the validation function
       />
     </div>
   );
