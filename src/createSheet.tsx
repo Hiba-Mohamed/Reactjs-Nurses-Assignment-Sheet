c5d4a6f941f2574ff55d8fcb772b77647bb3767a
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-07-30 18:04:43 -0230

    react hook form

--- src/createSheet.tsx
+++ src/createSheet.tsx
@@ -1,38 +1,59 @@
 import DatePickerComponent from "./datePickerComponent";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+  nurseName: string;
+  nurseBreak: number;
+  reliefName: string;
+  extraDuties: string;
+  fireCode: string;
+  patientName: string;
+  patientRoom: string;
+}
 
 export function CreateSheet() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
   return (
-    <div className="font-nunito relative flex flex-1 flex-col overflow-hidden justify-center items-center mb-12">
-      <div className="w-full max-w-xs">
-        <div>
-          <DatePickerComponent />
-        </div>
-        <div>
-          <h3 className="text-xl font-bold mb-4">Specify a Shift Type:</h3>
-          <div className="flex items-center justify-between">
-            <span className="ml-3 text-sm font-medium text-gray-900">
-              Day Shift
-            </span>
-            <label className="relative inline-flex items-center mr-5 cursor-pointer">
-              <input
-                type="checkbox"
-                value=""
-                className="sr-only peer"
-                checked
-              />
-              <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-purple-800"></div>
-              <span className="ml-3 text-sm font-medium text-gray-900">
-                purple-800
-              </span>
-            </label>
+    <div className="font-nunito relative overflow-hidden mb-12">
+      <p className="text-center font-bold text-xl my-4">
+        To get started, please select a shift date and type. Then add nurses and
+        patients.
+      </p>
+      <div className="flex flex-row flex-wrap justify-evenly">
+        <div className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 m-4">
+          <div className="mb-14">
+            <DatePickerComponent />
+          </div>
+          <div>
+            <h3 className="text-xl font-bold mb-4">Shift Type:</h3>
+            <div className="flex flex row items-center justify-evenly">
+              <button
+                className="bg-yellow hover:bg-dyellow text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Day Shift
+              </button>
+              <button
+                className="bg-purple hover:bg-dpurple text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Night Shift
+              </button>
+            </div>
           </div>
         </div>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 my-4">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 my-4"
+        >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Nurse's name
             </label>
             <input
+              {...register("nurseName", { required: true, maxLength: 20 })}
               type="text"
               className="mt-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200"
             ></input>
@@ -42,6 +63,7 @@ export function CreateSheet() {
               Nurse's break
             </label>
             <input
+              {...register("nurseBreak", { required: true, min: 1, max: 3 })}
               type="text"
               className="mt-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200"
             ></input>
@@ -51,6 +73,7 @@ export function CreateSheet() {
               Nurse's relief
             </label>
             <input
+              {...register("reliefName", { required: true, maxLength: 20 })}
               type="text"
               className="mt-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200"
             ></input>
@@ -60,6 +83,7 @@ export function CreateSheet() {
               Extra Duties
             </label>
             <input
+              {...register("extraDuties", { required: true, maxLength: 20 })}
               type="text"
               className="mt-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200"
             ></input>
@@ -69,6 +93,7 @@ export function CreateSheet() {
               Fire Code
             </label>
             <input
+              {...register("fireCode", { required: true, maxLength: 1 })}
               type="text"
               className="mt-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200"
             ></input>
@@ -80,12 +105,14 @@ export function CreateSheet() {
             </label>
             <div className="flex flex-row items-center my-2 appearance-none text-nunito-900 bg-white rounded-md block w-full p-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-nunito-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-nunito-200">
               <input
+                {...register("patientRoom", { required: true, maxLength: 20 })}
                 className="w-24 appearance-none focus:outline-none w-full"
                 type="text"
                 id="room"
                 placeholder="Room"
               ></input>
               <input
+                {...register("patientName", { required: true, maxLength: 20 })}
                 className="w-24 appearance-none focus:outline-none p-0"
                 type="text"
                 id="patient"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend:
-    {
-          colors:
-    {
-      'peach':'#EF798A',
-      'green':'#68B0AB',
-      'white':'#FFFFFF',
-      'lgreen':'#8FC0A9'
+    extend: {
+      colors: {
+        peach: "#EF798A",
+        green: "#68B0AB",
+        white: "#FFFFFF",
+        lgreen: "#8FC0A9",
+        yellow: "#fbbf24",
+        dyellow: "#d97706",
+        purple: "#86198f",
+        dpurple: "#1e1b4b",
+      },
+
+      boxShadow: {
+        lg: "1px 1px 6px rgba(0, 0, 0, 0.05), 1px 1px 6px rgba(0, 0, 0, 0.05);",
+      },
     },
-    
+
+    fontFamily: {
+      nunito: ['"Nunito"', "sans-serif"],
     },
-
-    fontFamily: 
-    {
-      'nunito': ['"Nunito"', 'sans-serif'],
-    },
-  
-
-
-
   },
 
-
   plugins: [],
-}
+};
 
