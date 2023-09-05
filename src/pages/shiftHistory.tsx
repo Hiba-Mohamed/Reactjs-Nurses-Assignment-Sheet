8070d77e97154a04ddda319a8009c61474275d8f
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-09-05 18:41:15 -0230

    Shift history initial layout structure

--- src/pages/shiftHistory.tsx
+++ src/pages/shiftHistory.tsx
@@ -1,5 +1,134 @@
 export function ShiftHistory() {
-    return(
-        <div>Shift History</div>
-    )
+  return (
+    <div className="bg-slate-100 font-nunito">
+      <div className="">
+        <div className="space-y-4 flex flex-col items-center w-screen mb-8">
+          <h1 className="text-center mt-10 text-4xl font-bold">Shift Record</h1>
+
+          <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
+            <div className="flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointer">
+              <span>Year</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+            <div className="flex rounded-lg text-gray-500 font-semibold cursor-pointer">
+              <span>Month</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+            <div className="flex rounded-lg text-gray-500 font-semibold cursor-pointer">
+              <span>Day</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+            <div className="flex rounded-lg text-gray-500 font-semibold cursor-pointer">
+              <span>Type</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+            <div className="bg-cyan-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+              <span>Search</span>
+            </div>
+          </div>
+
+          <div className="flex items-center p-6 space-x-6 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500">
+            <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 opacity-30"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                className="bg-gray-100 outline-none"
+                type="text"
+                placeholder="Article name or keyword..."
+              />
+            </div>
+            <div className="flex py-3 px-4 rounded-lg text-gray-500 font-semibold cursor-pointer">
+              <span>All categorie</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+            <div className="bg-yellow-400 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+              <span>Search</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
