ea313166fde44a8ad1f27bbf5d448673bf6c46f1
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-09-21 21:01:23 -0230

    styling

--- src/pages/editNurse.tsx
+++ src/pages/editNurse.tsx
@@ -2,8 +2,8 @@ import NurseInfoForm from "../components/nurseForm";
 
 export function EditNursePage(){
     return (
-      <div className="bg-slate-100 font-nunito">
-        <h1 className="text-center text-5xl font-bold my-8">Shift Record</h1>{" "}
+      <div className="bg-greygreen font-nunito">
+        <h1 className="text-center text-5xl font-bold p-8">You are editing a nurse</h1>{" "}
         <NurseInfoForm />
       </div>
     );
