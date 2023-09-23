93014e85902c93a30bd2bcf7882a1b9e7492fbe4
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-09-23 13:37:35 -0230

    styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,19 @@
 export function AboutPage() {
   return (
-    <div
-      className="bg-cover bg-repeat bg-opacity-10"
-      style={{ backgroundImage: `url('images/banner-pic.png')` }}
-    >
-      <div className="backdrop-blur-sm font-nunito flex flex-col relative max-w-5xl mx-auto pt-20 sm:pt-12 lg:pt-18">
-        <div className="flex flex-row items-center">
-          <img
-            className="w-44"
-            src="./images/NAS-logo.png"
-            alt="Nurses Assignment Sheet logo"
-          />
-          <h3 className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
+    <div className="bg-repeat bg-opacity-10">
+      <div className="flex justify-center">
+        <img src="./images/NAS-logo.png" alt="Nurses Assignment Sheet logo" />
+      </div>
+      <div className="font-nunito flex flex-row items-start">
+        <div className="flex flex-row items-center shrink">
+          <img className="w-44" src="./images/about1.png" alt="" />
+        </div>
+
+        <div className="text-nunito-400 tracking-tight p-4">
+          <h3 className="font-extrabold tracking-tight text-center">
             {" "}
             What is the NAS?
           </h3>
-        </div>
-
-        <div className="text-nunito-400 text-2xl sm:text-3xl lg:text-4xl tracking-tight p-4 text-grey opacity-60">
           <p className="my-4">
             The NAS (Nurses Assignment Sheet) is an electronic document that
             replaces the paper based nurses assignment sheet used in some
@@ -34,22 +30,26 @@ export function AboutPage() {
         </div>
       </div>
 
-      <div className="backdrop-blur-sm font-nunito flex flex-col relative max-w-5xl mx-auto pt-20 sm:pt-12 lg:pt-18">
-        <div className="flex flex-row items-center">
-          <img className="w-44" src="./images/why-pic.png" alt="A picture of question marks" />
-          <h3 className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
-            Why Use the NAS?
-          </h3>
+      <div className="font-nunito flex flex-row items-start">
+        <div className="flex flex-row items-start">
+          <img
+            className="w-44"
+            src="./images/about2.png"
+            alt="A picture of question marks"
+          />
         </div>
 
-        <div className="text-nunito-400 text-2xl sm:text-3xl lg:text-4xl tracking-tight p-4 text-grey">
+        <div className="text-nunito-400 tracking-tight p-4 text-grey">
+          <h3 className="text-nunito-900 font-extrabold tracking-tight text-center">
+            Why Use the NAS?
+          </h3>
           <p className="my-4">
             Using the NAS offers several advantages including, but not limited
             to:
           </p>
           <ul className="list">
             <p className="text-green font-bold">Efficiency:</p>
-            <li className="my-4 opacity-60">
+            <li className="my-4 ">
               Electronic assignment sheets streamline the process of assigning
               and communicating patient information to nurses. They eliminate
               the need for manual entry and allow for real-time updates,
@@ -57,7 +57,7 @@ export function AboutPage() {
             </li>
 
             <p className="text-green font-bold">Error Reduction:</p>
-            <li className="my-4 opacity-60">
+            <li className="my-4 ">
               Electronic nurses' assignment sheets contribute to a reduction in
               errors compared to their paper-based counterparts. With electronic
               systems, information can be entered and updated accurately,
@@ -66,7 +66,7 @@ export function AboutPage() {
             </li>
 
             <p className="text-green font-bold">Data Analysis and Reporting:</p>
-            <li className="my-4 opacity-60">
+            <li className="my-4 ">
               Electronic systems can capture and store data, allowing for
               analysis and reporting on patient outcomes, workload distribution,
               and overall unit performance. This data-driven approach helps
@@ -76,7 +76,7 @@ export function AboutPage() {
             </li>
 
             <p className="text-green font-bold">Environmental Impact:</p>
-            <li className="my-4 opacity-60">
+            <li className="my-4 ">
               Shifting from paper-based assignment sheets to electronic ones
               reduces paper consumption and promotes sustainability. This
               environmentally-friendly approach aligns with the goals of
@@ -88,18 +88,22 @@ export function AboutPage() {
       </div>
 
       <div>
-        <div className="backdrop-blur-sm font-nunito flex flex-col relative max-w-5xl mx-auto pt-20 sm:pt-12 lg:pt-18 my-8">
+        <div className="font-nunito flex flex-row items-start">
           <div className="flex flex-row items-center">
-            <img className="w-44" src="./images/feature-pic.png" alt="A picture of a light bulb" />
-            <h3 className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
-              The unique features of the NAS:
-            </h3>
+            <img
+              className="w-44"
+              src="./images/about3.png"
+              alt="A picture of a light bulb"
+            />
           </div>
 
           <div className="flex flex-col">
-            <div className="text-nunito-400 text-2xl sm:text-3xl lg:text-4xl tracking-tight p-4 text-grey">
+            <h3 className="text-nunito-900 font-extrabold tracking-tight text-center">
+              The unique features of the NAS:
+            </h3>
+            <div className="text-nunito-400 tracking-tight p-4 text-grey">
               <h4 className="text-green font-bold my-4">Regulated access</h4>
-              <p className="opacity-60">
+              <p className="">
                 Electronic nurses' assignment sheets offer regulated access,
                 ensuring that only authorized users can view and make changes to
                 the document. Access can be restricted to healthcare
@@ -109,11 +113,11 @@ export function AboutPage() {
               </p>
             </div>
 
-            <div className="text-nunito-400 text-2xl sm:text-3xl lg:text-4xl tracking-tight p-4 text-grey">
+            <div className="text-nunito-400 tracking-tight p-4 text-grey">
               <h4 className="text-green font-bold my-4">
                 Creating Electronic Sheets for Future Dates
               </h4>
-              <p className="opacity-60">
+              <p className="">
                 With the NAS, healthcare professionals can effortlessly generate
                 electronic sheets for future dates, enabling them to plan and
                 organize patient information in advance. This powerful feature
@@ -123,11 +127,11 @@ export function AboutPage() {
               </p>
             </div>
 
-            <div className="text-nunito-400 text-2xl sm:text-3xl lg:text-4xl tracking-tight p-4 text-grey">
+            <div className="text-nunito-400 tracking-tight p-4 text-grey">
               <h4 className="text-green font-bold my-4">
                 Duplication Safeguards
               </h4>
-              <p className="opacity-60">
+              <p className="">
                 Electronic nurses' assignment sheets incorporate safeguards to
                 prevent the duplication of nurses' names, patient names, or room
                 numbers. The electronic system can employ validation rules and
