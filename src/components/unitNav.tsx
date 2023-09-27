12359cde066d7089a8de220dad64d6dbaf89a7db
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-09-27 20:01:26 -0230

    unit nav mobile responsiveness completed

--- src/components/unitNav.tsx
+++ src/components/unitNav.tsx
@@ -1,27 +1,27 @@
 export default function UnitNav ()
 {
     return (
-      <div className="font-nunito">
-        <div className="flex flex-col items-center justify-center pb-10">
-          <h1 className="text-nunito-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center m-4">
+      <div className="font-nunito min-h-screen">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="pt-10 text-nunito-900 font-extrabold text-2xl sm:text-5xl lg:text-6xl tracking-tight text-center p-4">
             Welcome to Your Nursing Unit
           </h1>
-          <h2 className="text-nunito-900 font-extrabold text-xl sm:text-2xl lg:text-lg tracking-tight text-center m-10">
+          <h2 className="text-nunito-900 font-extrabold text-md sm:text-2xl  tracking-tight text-center p-10">
             Please select from the options below
           </h2>
         </div>
-        <div className="flex flex-row flex-wrap justify-evenly mt-10 mb-40">
+        <div className="flex flex-row flex-wrap justify-evenly sm:py-10 pb-10 gap-4">
           <div>
             <a
               href="/startSheet"
               className="flex flex-col items-center justify-center"
             >
               <img
-                className="w-40 rounded-lg "
+                className="sm:w-40 w-16 rounded-lg "
                 src="images/create-logo.png"
                 alt=""
               />
-              <p className="text-lg rounded-lg p-2 bg-white text-green font-bold mt-2">
+              <p className="text-md sm:text-lg rounded-lg p-2 bg-white text-green font-bold mt-2">
                 Create A New Shift
               </p>
             </a>
@@ -32,11 +32,11 @@ export default function UnitNav ()
               className="flex flex-col items-center justify-center"
             >
               <img
-                className="w-40 rounded-lg "
+                className="sm:w-40 w-16 rounded-lg "
                 src="images/history-logo.png"
                 alt=""
               />
-              <p className="text-lg rounded-lg p-2 bg-white text-green font-bold mt-2">
+              <p className="text-md sm:text-lg rounded-lg p-2 bg-white text-green font-bold mt-2">
                 Past/Future shifts
               </p>
             </a>
