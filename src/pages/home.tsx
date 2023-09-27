25a5b208926a929cac3819159ade59e95bfbdc33
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-09-27 13:51:43 -0230

    styling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ export function Home() {
       className="bg-cover bg-repeat bg-opacity-10 h-screen mx-auto"
       style={{ backgroundImage: `url('images/banner-pic.png')` }}
     >
-      <div className="font-nunito flex flex-row relative max-w-2xl sm:max-w-5xl mx-auto mb-16 pt-4 pt-24">
+      <div className="font-nunito flex flex-row relative max-w-2xl sm:max-w-5xl mx-auto mb-16 pt-20">
         <div className="flex flex-col items-center w-screen mb-8">
           <img
             src="./images/NAS-logo.png"
@@ -17,11 +17,11 @@ export function Home() {
             alt="A picture of two pictures, on the left two nurses in nursing station, and on the right a picture of a nurse caring for a patient"
           />
 
-          <p className="text-nunito-900 font-extrabold text-3xl md:text-5xl lg:text-6xl tracking-tight text-center p-4">
+          <p className="text-nunito-900 font-extrabold text-lg sm:text-4xl lg:text-6xl tracking-tight text-center p-4 max-w-sm sm:max-w-2xl">
             Generate electronic Nurses Assignment Sheets now using NAS
           </p>
 
-          <div className="flex items-center flex-col sm:flex-row gap-4">
+          <div className="flex items-center flex-col sm:flex-row gap-4 sm:pt-4 pt-10">
             <a
               className="bg-slate-200 hover:bg-slate-100 text-cyan-700 font-bold sm:py-4 sm:px-8 py-2 px-4 rounded lg:text-xl text-sm"
               href="/account"
