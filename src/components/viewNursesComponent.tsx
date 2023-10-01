ad29f24223bce2db8c835e209e3a551391aaf08d
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-01 11:56:41 -0230

    components made for view shift page from shift record page

---
+++ src/components/viewNursesComponent.tsx
@@ -0,0 +1,135 @@
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+interface IPatientData {
+  patientName: string;
+  patientRoom: string;
+}
+
+
+export function ViewNurseCard({ staffData }: { staffData: any }) {
+  const { ShiftId } = useParams();
+  const navigate = useNavigate();
+  console.log(staffData);
+
+  const deleteNurse = (ShiftId: any, nurseId: string) => {
+    // Retrieve shift data array from localStorage
+    const existingDataJSON = localStorage.getItem("startShiftDataArray");
+    const existingData = existingDataJSON ? JSON.parse(existingDataJSON) : [];
+
+    console.log("existing Data", existingData);
+
+    // Find the shift data object with the matching shiftId
+    const matchingData = existingData.find(
+      (data: any) => data.ShiftId === ShiftId
+    );
+
+    console.log("matching Data:", matchingData);
+
+    const exsitingNurseArray = matchingData.staff;
+    // get the index of the nurse card
+    exsitingNurseArray.splice(nurseId, 1);
+
+    const updatedNurseList = exsitingNurseArray;
+    console.log("Updated Nurse List", updatedNurseList);
+    //  update the local storage
+    localStorage.setItem("startShiftDataArray", JSON.stringify(existingData));
+    window.location.reload();
+  };
+
+  const editNurse = (ShiftId: any, nurseId: string) => {
+    navigate(`/editNurse/${ShiftId}/${nurseId}`);
+
+    // 2- have the form autopopulated with the nurse info using nurseId
+    // 3- on form submission is handled through the onSubmitEdit function that is in the editNurse page.
+    // 4- have to find a way to send the  ShiftId to the editNurse page to be passed in as a prop there.
+  };
+  console.log(staffData);
+  if (ShiftId && staffData.length !== 0) {
+    return (
+      <div className="flex flex-row flex-wrap justify-evenly">
+        {staffData?.map((staffData: any, nurseIndex: number) => (
+          <div className="bg-white shadow-lg rounded-lg sm:px-8 sm:pt-6 sm:pb-8 my-4  max-w-sm mx-2 text-sm  lg:text-l sm:text-md">
+            <div key={nurseIndex} className="flex flex-col m-4">
+              <div className="flex flex-col justify-center items-center text-center font-bold">
+                {staffData.nurseData.nurseName}
+              </div>
+              <table className="table-auto">
+                <tbody>
+                  <tr>
+                    <td className="font-semibold text-cyan-700">Break:</td>
+                    <td>{staffData.nurseData.nurseBreak}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-semibold text-cyan-700">Relief:</td>
+                    <td>{staffData.nurseData.reliefName}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-semibold text-cyan-700">
+                      Extra Duties:
+                    </td>
+                    <td>{staffData.nurseData.extraDuties}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-semibold text-cyan-700">Fire Code:</td>
+                    <td className="text-red-500">
+                      {staffData.nurseData.fireCode}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              {staffData.nurseData.assignedPatient.map(
+                (patient: IPatientData, patientIndex: number) => (
+                  <div>
+                    <table className="mb-4">
+                      <thead>
+                        <tr className="border border-stone-700 bg-stone-400 text-white">
+                          <th className="border border-stone-700 px-2 py-1">
+                            Room
+                          </th>
+                          <th className="border border-stone-700 px-2 py-1">
+                            Patient
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr key={patientIndex}>
+                          <td className="border px-2 py-1">
+                            {patient.patientRoom}
+                          </td>
+                          <td className="border px-2 py-1">
+                            {patient.patientName}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                )
+              )}
+
+              <div className="flex flex-row justify-evenly items-center">
+                <button
+                  className="bg-sky-600 hover:bg-sky-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => editNurse(ShiftId, staffData.nurseId)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => deleteNurse(ShiftId, staffData.nurseId)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    console.log("no nurses added to this shift yet");
+  }
+}
+
+export default ViewNurseCard;
---
+++ src/pages/viewShift.tsx
@@ -0,0 +1,136 @@
+import NurseInfoForm from "../components/nurseForm";
+import { useParams } from "react-router-dom";
+import NurseCardDisplay from "../components/nurseCardDisplay";
+import { v4 as uuidv4 } from "uuid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IPatientData {
+  patientName: string;
+  patientRoom: string;
+}
+
+interface IFormInput {
+  nurseName: string;
+  nurseBreak: string;
+  reliefName: string;
+  extraDuties: string;
+  fireCode: string;
+  assignedPatient: IPatientData[];
+}
+
+function formatDate(dateString: string): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
+function retriveShiftDataLSwithShiftId(ShiftId: string): any {
+  // Retrieve shift data array from localStorage
+  const existingDataJSON = localStorage.getItem("startShiftDataArray");
+  const existingData = existingDataJSON ? JSON.parse(existingDataJSON) : [];
+
+  console.log("existing Data", existingData);
+
+  // Find the shift data object with the matching shiftId
+  const matchingData = existingData.find(
+    (data: any) => data.ShiftId === ShiftId
+  );
+
+  console.log("matching Data:", matchingData);
+
+  return matchingData ? matchingData.data : null;
+}
+
+export function ViewShift() {
+  const { ShiftId } = useParams();
+  const nurseId = uuidv4();
+
+  const form = useForm<IFormInput>();
+
+  const onSubmitForm: SubmitHandler<IFormInput> = (nurseData, event) => {
+    event?.preventDefault();
+    makeAndAddNurseDataToLS(nurseData);
+    form.reset();
+    console.log(nurseData);
+  };
+
+  function makeAndAddNurseDataToLS(nurseData: IFormInput) {
+    // Retrieve the existing shift data array from localStorage
+    const existingDataJSON = localStorage.getItem("startShiftDataArray");
+    const existingData = existingDataJSON ? JSON.parse(existingDataJSON) : [];
+
+    // Find the shift data object with the matching ShiftId
+    const matchingDataIndex = existingData.findIndex(
+      (data: any) => data.ShiftId === ShiftId
+    );
+
+    if (matchingDataIndex !== -1) {
+      // If a matching shift data is found, update its "staff" property
+      existingData[matchingDataIndex].staff =
+        existingData[matchingDataIndex].staff || [];
+      existingData[matchingDataIndex].staff.push({ nurseId, nurseData });
+
+      // Update the localStorage with the modified data
+      localStorage.setItem("startShiftDataArray", JSON.stringify(existingData));
+    } else {
+      console.log("Matching shift data not found for the provided ShiftId.");
+    }
+  }
+
+  if (ShiftId) {
+    // Check if ShiftId is defined
+    const shiftData = retriveShiftDataLSwithShiftId(ShiftId);
+
+    console.log("shiftData", shiftData);
+
+    console.log(shiftData);
+    if (ShiftId) {
+      const existingDataJSON = localStorage.getItem("startShiftDataArray");
+      const existingData = existingDataJSON ? JSON.parse(existingDataJSON) : [];
+      const matchingData = existingData.find(
+        (data: any) => data.ShiftId === ShiftId
+      );
+
+      const staffData = matchingData.staff ?? [];
+
+      console.log("staffData", staffData);
+
+      return (
+        <div className="font-nunito bg-greygreen sm:max-w-full">
+          <div className="flex flex-col items-center justify-center">
+            <div className="text-nunito-900 font-extrabold text-md sm:text-xl lg:text-3xl tracking-tight text-center p-2 bg-white shadow-lg rounded-lg sm:px-8 sm:pt-6 sm:pb-8 sm:py-4 m-8 text-green">
+              <p>{shiftData.unitName}</p>
+              <div className="flex sm:flex-row items-center flex-col">
+                {" "}
+                <p className="px-4">{formatDate(shiftData?.shiftDate)}</p>
+                <p className="px-4">{shiftData.shiftType}</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {" "}
+            <NurseCardDisplay staffData={staffData} />{" "}
+          </div>
+          <div>
+            {" "}
+            <NurseInfoForm
+              onSubmit={onSubmitForm}
+              Shifturl={ShiftId}
+              form={form}
+            />
+          </div>
+        </div>
+      );
+    } else {
+      console.log("ShiftId is undefined.");
+    }
+  } else {
+    console.log("ShiftId is undefined.");
+  }
+}
+
+export default ViewShift;
