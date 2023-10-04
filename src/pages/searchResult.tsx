31568267970ee516136cbbb284ea7757ef60daae
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-04 17:01:48 -0230

    conditional rendering

--- src/pages/searchResult.tsx
+++ src/pages/searchResult.tsx
@@ -11,12 +11,27 @@ const { shiftDate, shiftType } = useParams();
   );
 
   console.log("matching shift", matchingShift)
+  if(matchingShift){
   return (
     <div>
       <h1>{shiftDate}</h1>
       <h1>{shiftType}</h1>
     </div>
   );
+  }
+
+  else{
+    return (
+    <div className="flex flex-col bg-slate-100 items-center min-h-screen">
+      <h1 className="font-nunito text-center text-2xl sm:text-4xl font-bold py-8 items-center">
+        Search Result
+      </h1>{" "}
+      <div className="items-center flex w-full justify-evenly">
+        <img src="./images/searcherror.png" alt="" />
+      </div>
+    </div>
+  );}
+
 };
 
 export default SearchResults;
