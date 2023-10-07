2199c3750f018cf5cc5dee3e0856030d1f2dc4d1
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-07 15:29:38 -0230

    styling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -30,7 +30,7 @@ export function Home() {
             </a>
             <a
               href="/login"
-              className="bg-cyan-700 hover:bg-cyan-600 text-white font-bold sm:py-4 sm:px-8 py-2 px-4 rounded lg:text-xl text-sm"
+              className="bg-cyan-700 hover:bg-cyan-600 text-white font-bold sm:py-4 sm:px-8 py-2 px-12 rounded lg:text-xl text-sm"
             >
               Login
             </a>
