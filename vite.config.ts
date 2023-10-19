e8d9a88ee7a4b6f75a88a7b3e77700b7de605040
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-19 17:35:59 -0230

    intropage, corrected path to output css file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Reactjs-Nurses-Assignment-Sheet/', // Set the desired base path
+
 })
