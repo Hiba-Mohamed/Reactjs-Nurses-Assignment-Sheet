7f1c7ef69908b325cf6c6f7b3aac5666fbf4421e
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-19 21:45:08 -0230

    Revert "Update main.tsx"
    This reverts commit 2aaf659f6248c54fe6f4dd313fcf017cb0eefeab.

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   );
 } else {
