a9885545a7bf0d3b68b687f42402e53f8f2f8aed
Mitchell Hynes <me@mitchellhynes.com>
2023-10-22 20:13:34 -0230

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reactjs-Nurses-Assignment-Sheet/', // Set the desired base path
+  base: '/Reactjs-Nurses-Assignment-Sheet/',
   optimizeDeps: {
     include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx'],
   },
