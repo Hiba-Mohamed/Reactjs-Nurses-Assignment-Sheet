c5e5f642b2aadbb1896055546feb71f2ca4e3349
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-11-21 21:00:28 -0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reactjs-Nurses-Assignment-Sheet/',
+  base: 'https://github.com/Hiba-Mohamed/Reactjs-Nurses-Assignment-Sheet/',
   optimizeDeps: {
     include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx'],
   },
